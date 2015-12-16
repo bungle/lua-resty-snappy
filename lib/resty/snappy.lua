98fd513da9780c242da450a4f905a838c9d37424
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-12-17 00:18:59 +0200

    Cleanups.

--- lib/resty/snappy.lua
+++ lib/resty/snappy.lua
@@ -5,6 +5,7 @@ local ffi_cdef   = ffi.cdef
 local ffi_load   = ffi.load
 local ffi_str    = ffi.string
 local C          = ffi.C
+local tonumber   = tonumber
 
 ffi_cdef[[
 typedef enum {
@@ -19,19 +20,19 @@ snappy_status snappy_uncompressed_length(const char* compressed, size_t compress
 snappy_status snappy_validate_compressed_buffer(const char* compressed, size_t compressed_length);
 ]]
 
-local libsnappy = ffi_load("libsnappy")
-local char_t = ffi_typeof("char[?]")
-local size_t = ffi_typeof("size_t[1]")
+local lib = ffi_load "snappy"
+local char_t = ffi_typeof "char[?]"
+local size_t = ffi_typeof "size_t[1]"
 local snappy = {}
 
 function snappy.compress(input)
-    local input_length = #input
-    local max_length = snappy.max_compressed_length(input_length)
-    local compressed = ffi_new(char_t, max_length)
-    local compressed_length = ffi_new(size_t)
-    compressed_length[0] = tonumber(max_length)
-    local status = libsnappy.snappy_compress(input, input_length, compressed, compressed_length)
-    local len = tonumber(compressed_length[0])
+    local il = #input
+    local ml = snappy.max_compressed_length(il)
+    local compressed = ffi_new(char_t, ml)
+    local cl = ffi_new(size_t)
+    cl[0] = tonumber(ml)
+    local status = lib.snappy_compress(input, il, compressed, cl)
+    local len = tonumber(cl[0])
     if (status == C.SNAPPY_OK) then
         return ffi_str(compressed, len), len
     else
@@ -40,15 +41,15 @@ function snappy.compress(input)
 end
 
 function snappy.uncompress(compressed)
-    local compressed_length   = #compressed
-    local uncompressed_length = ffi_new(size_t)
-    local status = libsnappy.snappy_uncompressed_length(compressed, compressed_length, uncompressed_length)
+    local cl = #compressed
+    local ul = ffi_new(size_t)
+    local status = lib.snappy_uncompressed_length(compressed, cl, ul)
     if (status ~= C.SNAPPY_OK) then
         return nil, tonumber(status)
     end
-    local uncompressed = ffi_new(char_t, tonumber(uncompressed_length[0]))
-    status = libsnappy.snappy_uncompress(compressed, compressed_length, uncompressed, uncompressed_length)
-    local len = tonumber(uncompressed_length[0])
+    local uncompressed = ffi_new(char_t, tonumber(ul[0]))
+    status = lib.snappy_uncompress(compressed, cl, uncompressed, ul)
+    local len = tonumber(ul[0])
     if (status == C.SNAPPY_OK) then
         return ffi_str(uncompressed, len), len
     else
@@ -57,12 +58,12 @@ function snappy.uncompress(compressed)
 end
 
 function snappy.max_compressed_length(source_length)
-    return tonumber(libsnappy.snappy_max_compressed_length(source_length))
+    return tonumber(lib.snappy_max_compressed_length(source_length))
 end
 
 function snappy.uncompressed_length(compressed)
     local result = ffi_new(size_t)
-    local status = libsnappy.snappy_uncompressed_length(compressed, #compressed, result)
+    local status = lib.snappy_uncompressed_length(compressed, #compressed, result)
     if (status == C.SNAPPY_OK) then
         return tonumber(result[0])
     else
@@ -71,7 +72,7 @@ function snappy.uncompressed_length(compressed)
 end
 
 function snappy.validate_compressed_buffer(compressed)
-    local status = libsnappy.snappy_validate_compressed_buffer(compressed, #compressed)
+    local status = lib.snappy_validate_compressed_buffer(compressed, #compressed)
     return status == C.SNAPPY_OK
 end
 
